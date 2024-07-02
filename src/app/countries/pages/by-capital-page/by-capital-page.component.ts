65911f8c90fc50af9616105156ca3313dd7dbeea
Francesc Alberola <francesc.alberola@nter.es>
2024-07-02 13:29:43 +0200

    fin section 10

--- src/app/countries/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CountriesService } from '../../services/countries.service';
 import { Country } from '../../interfaces/country';
 
@@ -7,13 +7,18 @@ import { Country } from '../../interfaces/country';
   templateUrl: './by-capital-page.component.html',
   styles: ``
 })
-export class ByCapitalPageComponent {
+export class ByCapitalPageComponent implements OnInit {
 
   public countries: Country[] = [];
-
   public isLoading: boolean = false;
+  public initialValue: string = '';
 
   constructor( private CountriesService: CountriesService ) { }
+
+  ngOnInit(): void {
+    this.countries = this.CountriesService.cacheStore.byCapital.countries;
+    this.initialValue = this.CountriesService.cacheStore.byCapital.term;
+  }
 
   searchByCapital(term: string): void {
 
